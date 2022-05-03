a871d7774353cbd0c73f37002489118503dc2f50
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2022-05-03 15:41:42 +0700

    scrool to the end of page

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-import requests
+import time
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('https://baomoi.com/')
+driver = webdriver.Chrome()
+driver.get("https://m.baomoi.com/trang1.epi")
 
-req = requests.get('https://baomoi.com/')
-soup = BeautifulSoup(browser.page_source, "lxml")
+time.sleep(5)
 
-for link in soup.find_all(class_="bm_F"):
+SCROLL_PAUSE_TIME = 2
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+soup = BeautifulSoup(driver.page_source, "lxml")
+
+for link in soup.find_all(class_="bm_Cz"):
     num_related_articles = int(link.get_text().replace(" liên quan", ""))
     if 1 < num_related_articles < 30:
         print(link.get('href') + ": " + str(num_related_articles))
